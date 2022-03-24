69ad9f3ab49e04236afda11e4fef8598bced561f
SPISHIO <Sibusisosigudla21@gmail.com>
2022-03-24 11:45:31 +0200

    added

--- python.py
+++ python.py
@@ -5,5 +5,8 @@ with open ("path_to_and_name_of_the_file","mode") as variable_nme:
     counter= counter+10
 
     User_name= "Sibusiso SIGUDLA"
-    user_id= "SPISHO"
+    user_id= 0749073536
+    Message= " I AM LEARING TO USE BASH AND PYTHON"
+    CONLUSION= "WALK WITH ME ON THIS JOURNY"
+    print(" I am  DEVELOPER")
 
