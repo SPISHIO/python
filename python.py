a3414fbf03f4226acde7e1bdf633040de8b69360
SPISHIO <Sibusisosigudla21@gmail.com>
2022-03-24 11:31:51 +0200

    update

--- python.py
+++ python.py
@@ -1,2 +1,9 @@
 with open ("path_to_and_name_of_the_file","mode") as variable_nme:
     variable_name.write('I AM A PYTHON KING')
+
+    counter=10
+    counter= counter+10
+
+    User_name= "Sibusiso SIGUDLA"
+    user_id= "SPISHO"
+
